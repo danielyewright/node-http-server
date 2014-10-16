1a10fafc8c7eb35581e051e47282988a75e1949b
Daniely Wright <danielywrighte@gmail.com>
2014-10-15 21:48:16 -0400

    Updated server.js and README.md

--- server.js
+++ server.js
@@ -1,27 +1,31 @@
-var http = require("http"),
-    url = require("url"),
-    path = require("path"),
-    fs = require("fs"),
-    port = process.argv[2] || 8888;
-    port = 3000;
+var http = require("http"); 
+var url = require("url");
+var path = require("path");
+var fs = require("fs");
 
+// Specify the port to use, default is 3000
+var port = process.argv[2] || 3000;
+
+// Create server
 http.createServer(function(request, response) {
-
-  var uri = url.parse(request.url).pathname
-    , filename = path.join(process.cwd(), uri);
+  
+  var uri = url.parse(request.url).pathname, filename = path.join(process.cwd(), uri);
 
   fs.exists(filename, function(exists) {
-    if(!exists) {
+    
+    if (!exists) {
       response.writeHead(404, {"Content-Type": "text/html"});
       response.write("404 Not Found\n");
       response.end();
       return;
     }
 
+    // Default static HTML file to load
     if (fs.statSync(filename).isDirectory()) filename += 'index.html';
 
     fs.readFile(filename, "binary", function(err, file) {
-      if(err) {        
+      
+      if (err) {        
         response.writeHead(500, {"Content-Type": "text/html"});
         response.write(err + "\n");
         response.end();
@@ -31,8 +35,11 @@ http.createServer(function(request, response) {
       response.writeHead(200);
       response.write(file, "binary");
       response.end();
+      
     });
+    
   });
+  
 }).listen(parseInt(port, 10));
 
 console.log("Server running at http://127.0.0.1:" + port + "/\nCTRL + C to shutdown");
